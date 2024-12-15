6356a18c40d2778362c4abbf6f6c76026b057328
Abdul Ahad <abdulahad1028@gmail.com>
2024-12-15 13:58:54 +0500

    allow cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,7 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Set("cors_mode", "no-cors")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
